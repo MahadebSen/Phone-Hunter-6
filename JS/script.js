8ce9bb703164d85d309c9ddf7b291b570f529036
Mahadeb Sen <mahadebsen237@gmail.com>
2022-03-02 06:00:02 +0530

    almost complete

--- JS/script.js
+++ JS/script.js
@@ -1,31 +1,46 @@
 
 document.getElementById("search-btn").addEventListener('click', function(){
     const searchInput = document.getElementById('search-input');
-    // console.log(searchInput.value)
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchInput.value}`
-    fetch(url)
-    .then(res => res.json())
-    .then(data => searchResult(data.data))
-    searchInput.value = '';
+    
+    if (searchInput.value === ''){
+        document.getElementById('product-details').innerHTML =  `
+        <p class="text-center my-auto">Please write somthing in search field</p>`;
+        document.getElementById('search-result').innerHTML = '';
+    }
+    else{
+        document.getElementById('product-details').innerHTML = '';
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchInput.value}`
+        fetch(url)
+        .then(res => res.json())
+        .then(data => searchResult(data))
+        searchInput.value = '';
+    }
 });
 
-const searchResult = data => {
+const searchResult = searchData => {
     const searchResult = document.getElementById('search-result');
     searchResult.innerHTML = '';
-    for (const product of data){
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
-        <div class="card h-100 p-3">
-            <img src="${product.image}" class="card-img-top" alt="">
-            <div class="card-body">
-                <h5 class="card-title">${product.phone_name}</h5>
-                <h6>${product.brand}</h6>
-            </div>
-            <button onclick="detailsCall('${product.slug}')" type="button" class="btn btn-outline-primary w-50">Details</button>
-        </div>`;
-        searchResult.appendChild(div);
-    };
+    if (searchData.status === false){
+        document.getElementById('product-details').innerHTML =  `
+        <p class="text-center mt-5 pt-5 fw-bold">No search result found</p>`;
+    }
+    else{
+        
+        for (const product of searchData.data){
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+            <div class="card h-100 p-3">
+                <img src="${product.image}" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">${product.phone_name}</h5>
+                    <h6>${product.brand}</h6>
+                </div>
+                <button onclick="detailsCall('${product.slug}')" type="button" class="btn btn-outline-primary w-50">Details</button>
+            </div>`;
+            searchResult.appendChild(div);
+        };
+    }
 };
 
 const detailsCall = id =>{
@@ -52,7 +67,7 @@ const details = features =>{
     <div class="card-body">
         <h4 class="card-title">${features.name}</h4>
         <h5 class="card-title"><span class="fw-bold">Brand: </span>${features.brand}</h5>
-        <p class="card-text"><span class="fw-bold">Release Date: </span>${features.releaseDate}</p>
+        <p id="release-date" class="card-text"><span class="fw-bold">Release Date: </span>${releaseDate(features.releaseDate)}</p>
         <p class="card-text"><span class="fw-bold">Main Features: </span>
             <ul>
                 <li><span class="fw-bold">Chipset:  </span>${features.mainFeatures.chipSet}</li>
@@ -79,6 +94,19 @@ const details = features =>{
     productDetails.appendChild(div2);
 };
 
-// const sensors = a =>{
+const releaseDate = date =>{
+    if (date == ""){
+        const releaseDateData = 'No release date found';
+        return releaseDateData;
+    }
+    else{
+        const releaseDateData2 = date;
+        return releaseDateData2;
+    };
+};
 
-// }
+
+
+
+{/* <p class="card-text"><span class="fw-bold">Release Date: </span>${features.releaseDate}</p> */}
+// ${releaseDate(features.releaseDate)}
